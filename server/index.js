cdcd00e2efeba8a1c924aff942008ae69b3e4734
Ariel <ariel.agawin.calipay@gmail.com>
2023-09-10 13:53:01 +0800

    Env File and backend code revision

--- server/index.js
+++ server/index.js
@@ -1,44 +1,47 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3002;
-const app_pass = 'u d x m g r y k t n z y u e z j';
 
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+    origin: 'http://your-frontend-app-url.com', // Change this to your frontend app's URL
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'ariel.agawin.calipay@gmail.com',
-        pass: app_pass
-    }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 app.post('/send-email', (req, res) => {
     const { firstName, lastName, phone, email, message } = req.body;
 
     const mailOptions = {
-        from: 'ariel.agawin.calipay@gmail.com',
+        from: process.env.EMAIL_USER,
         to: 'arielcalipay2@gmail.com',
         subject: 'Get In Touch',
-        html: `<h4><strong>Name: </strong>${firstName} ${lastName}</h4><h4><strong>Phone: </strong>${phone}</h4><h4><strong>Email: </strong>${email}</h4><h4><strong>Message: </strong>${message}</h4>`
-    }
+        html: `<h4><strong>Name: </strong>${firstName} ${lastName}</h4><h4><strong>Phone: </strong>${phone}</h4><h4><strong>Email: </strong>${email}</h4><h4><strong>Message: </strong>${message}</h4>`,
+    };
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error(error);
-            res.status(500).send('Error on sending email');
+            res.status(500).json({ error: 'Error on sending email' });
         } else {
-            console.log('Email sent: ', info.response);
-            res.status(200).send('Email sent successfully');
+            console.log('Email sent:', info.response);
+            res.status(200).json({ message: 'Email sent successfully' });
         }
-    })
-
+    });
 });
 
 app.listen(port, () => {
